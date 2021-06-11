cc106b129a13db717998c18c455d2436c79f3084
hebaxi <914290709@qq.com>
2021-06-11 13:35:31 +0800

    麻烦

--- EXCEL/merge.py
+++ EXCEL/merge.py
@@ -4,15 +4,18 @@ import numpy as np
 import os
 import time
 
+#  主要是简单合并excel， 太多了。麻烦的很，不想搞 。
 # 地址
-
+# 修改一下，换一个新的保存地址。最好加上时间
 
 path = r"C:\Users\1\Desktop\工作\202106\test"
 
-# 当前时间
-time = time.strftime("%Y-%m-%d", time.localtime())
+path1 = r"C:\Users\1\Desktop\工作\202106\save"
 
-print(path + '\\' + time + '.xlsx')
+# 当前时间格式，很有用。
+tim = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+# hour = time.strftime("%H:%M", time.localtime())
+# print(path + '\\' + tim + '.xlsx')
 # print(a)
 
 filenames = os.listdir(path)
@@ -24,7 +27,7 @@ for i in filenames:
     data = pd.read_excel(path + '\\' + i)
     df = df.append(data)
 
-adres = path + '\\' + time + '.' + 'xlsx'
+adres = path1 + '\\' + tim + '.xlsx'
 
+print(adres)
 df.to_excel(adres)
-
